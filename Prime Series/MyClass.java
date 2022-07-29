ec580b4e6b1e54e864c8f4af5a970f16e8012413
Vignesh E <evicky2002@gmail.com>
2022-07-30 00:25:27 +0530

    practice

--- Prime Series/MyClass.java
+++ Prime Series/MyClass.java
@@ -1,4 +1,23 @@
 public class MyClass {
+    static void primeRange(int n, int m) {
+        if (n == 1) {
+            n++;
+        }
+        int start = n;
+        while (start <= m) {
+            boolean isPrime = true;
+            for (int i = 2; i <= start / i; i++) {
+                if (start % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println(start);
+            }
+            start++;
+        }
+    }
+
     static void primeSeries(int n) {
         if (n == 1) {
             System.out.println("Nor a Prime or composite");
@@ -21,6 +40,7 @@ public class MyClass {
     }
 
     public static void main(String[] args) {
-        primeSeries(7);
+        // primeSeries(7);
+        primeRange(10, 30);
     }
 }
