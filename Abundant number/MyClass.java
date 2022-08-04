396f117b9b9887104b4f78dfa919c75e23c54263
Vignesh E <evicky2002@gmail.com>
2022-08-04 05:31:41 +0530

    practice

---
+++ Abundant number/MyClass.java
@@ -0,0 +1,21 @@
+//A Number that is smaller than the sum of all it's factors except the number itself is known as an Abundant number.
+public class MyClass {
+    static boolean isAbundant(int n) {
+        boolean isAbundant = false;
+        int result = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                result += i;
+            }
+        }
+        if (n < result) {
+            isAbundant = true;
+        }
+
+        return isAbundant;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAbundant(13));
+    }
+}
