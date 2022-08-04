8135cf7fb9e033c681cd45fde500ea97fcb000f0
Vignesh E <evicky2002@gmail.com>
2022-08-04 05:51:06 +0530

    practice

---
+++ Greatest Common Divisor/MyClass.java
@@ -0,0 +1,17 @@
+// use repeated subtraction to arrive at GCD
+public class MyClass {
+    static int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(5, 25));
+    }
+}
